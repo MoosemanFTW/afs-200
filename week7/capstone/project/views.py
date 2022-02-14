1fbdf7ad0e2697a9d6e754660a811e8640b38c47
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2022-02-13 18:44:32 -0800

    save point

--- week7/capstone/project/views.py
+++ week7/capstone/project/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'regestration/signup.html'
+    template_name = 'registration/signup.html'
 
 
 def home(request):
